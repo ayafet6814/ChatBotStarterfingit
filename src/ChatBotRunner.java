0174acd5401a856fefebc173de6532f04d97391f
BT_1N3_19@bktech.local <BT_1N3_19@bktech.local>
2018-10-19 10:46:55 -0400

    Initial commit

---
+++ src/ChatBotRunner.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+/**
+ * A simple class to run our chatbot teams.
+ * @author Brooklyn Tech CS Department
+ * @version September 2018
+			*/
+		public class ChatBotRunner
+		{
+
+			/**
+			 * Create instances of each chatbot, give it user input, and print its replies. Switch chatbot responses based on which chatbot the user is speaking too.
+			 */
+			public static void main(String[] args)
+			{
+				ChatBot1 chatbot1 = new ChatBot1();
+				ChatBot2 chatbot2 = new ChatBot2();
+				ChatBot3 chatbot3 = new ChatBot3();
+				ChatBot4 chatbot4 = new ChatBot4();
+
+
+				Scanner in = new Scanner (System.in);
+				System.out.println("Hi there. Welcome to ChatBot! What would you like to talk about??? (1) Clothing --- (2) Music --- (3) Breakfast --- (4) Weather");
+				int statement = in.nextInt();
+				if (statement == 1)
+				{
+					chatbot1.chatLoop("1");
+				}
+				else if (statement == 2)
+				{
+					chatbot2.chatLoop("2");
+				}
+				else if (statement == 3)
+				{
+					chatbot3.chatLoop("3");
+				}
+				else if (statement == 4)
+				{
+					chatbot4.chatLoop("4");
+				}
+
+		}
+	}
